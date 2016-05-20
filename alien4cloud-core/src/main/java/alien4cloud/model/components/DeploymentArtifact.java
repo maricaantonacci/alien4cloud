fae1d44fff71b07dfafc634e066128006d095e46
Bassi Matteo <bassimatteo88@gmail.com>
2016-05-20 17:40:21 +0200

    Improve Tosca parser
    - Artifact "file" property can be a complex type (i.e. function)
    - Velocity template can print Artifact

--- alien4cloud-core/src/main/java/alien4cloud/model/components/DeploymentArtifact.java
+++ alien4cloud-core/src/main/java/alien4cloud/model/components/DeploymentArtifact.java
@@ -13,10 +13,11 @@ public class DeploymentArtifact implements IArtifact {
     @Getter
     private String artifactRef;
     /** Specifies the display name of the artifact. */
+    @Getter
     private String artifactName;
 
     @Getter
-    private String file;
+    private AbstractPropertyValue file;
     
     /**
      * Non TOSCA compliant property, the artifactRepository indicate where the artifact is stored. It might be in the archive it-self (in this case this
--- alien4cloud-core/src/main/java/alien4cloud/tosca/parser/impl/advanced/ComplexPropertyValueParser.java
+++ alien4cloud-core/src/main/java/alien4cloud/tosca/parser/impl/advanced/ComplexPropertyValueParser.java
@@ -1,5 +1,6 @@
 package alien4cloud.tosca.parser.impl.advanced;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.PascalCaseStrategy;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
@@ -18,6 +19,7 @@ import alien4cloud.model.components.AbstractPropertyValue;
 import alien4cloud.model.components.ComplexPropertyValue;
 import alien4cloud.model.components.ListPropertyValue;
 import alien4cloud.model.components.ScalarPropertyValue;
+import alien4cloud.tosca.parser.INodeParser;
 import alien4cloud.tosca.parser.ParserUtils;
 import alien4cloud.tosca.parser.ParsingContextExecution;
 import alien4cloud.tosca.parser.mapping.DefaultParser;
@@ -25,29 +27,36 @@ import alien4cloud.tosca.parser.mapping.DefaultParser;
 @Component
 public class ComplexPropertyValueParser extends DefaultParser<AbstractPropertyValue> {
 
-    @Override
-    public AbstractPropertyValue parse(Node node, ParsingContextExecution context) {
-        AbstractPropertyValue parsedValue;
-        if (node instanceof MappingNode) {
-            Map<String, Object> result = Maps.newHashMap();
-            for (NodeTuple innernode : ((MappingNode) node).getValue()) {
-                AbstractPropertyValue parsedInnerValue = parse(innernode.getValueNode(), context);
-                result.put(ParserUtils.getScalar(innernode.getKeyNode(), context), parsedInnerValue);
-            }
-            parsedValue = new ComplexPropertyValue(result);
-        } else if (node instanceof SequenceNode) {
-            List<Object> result = Lists.newArrayList();
-            for (Node innernode : ((SequenceNode) node).getValue()) {
-                AbstractPropertyValue parsedInnerValue = parse(innernode, context);
-                result.add(parsedInnerValue);
-            }
-            parsedValue = new ListPropertyValue(result);
-        } else if (node instanceof ScalarNode) {
-            parsedValue = new ScalarPropertyValue(((ScalarNode) node).getValue());
-        } else {
-            throw new InvalidArgumentException("Do not expect other node than MappingNode or SequenceNode here " + node.getClass().getName());
-        }
-        parsedValue.setPrintable(true);
-        return parsedValue;
+  @Override
+  public AbstractPropertyValue parse(Node node, ParsingContextExecution context) {
+    AbstractPropertyValue parsedValue;
+    if (node instanceof MappingNode) {
+      // get_input, get_property or get_attribute calls the
+
+      Map<String, Object> result = Maps.newHashMap();
+      for (NodeTuple innernode : ((MappingNode) node).getValue()) {
+        INodeParser<AbstractPropertyValue> parser =
+            context.getRegistry().get("node_template_property");
+        AbstractPropertyValue parsedInnerValue = parser.parse(innernode.getValueNode(), context);
+        parsedInnerValue.setPrintable(true);
+        result.put(ParserUtils.getScalar(innernode.getKeyNode(), context), parsedInnerValue);
+      }
+      parsedValue = new ComplexPropertyValue(result);
+    } else if (node instanceof SequenceNode) {
+      List<Object> result = Lists.newArrayList();
+      for (Node innernode : ((SequenceNode) node).getValue()) {
+        AbstractPropertyValue parsedInnerValue = parse(innernode, context);
+        result.add(parsedInnerValue);
+      }
+      parsedValue = new ListPropertyValue(result);
+    } else if (node instanceof ScalarNode) {
+      parsedValue = new ScalarPropertyValue(((ScalarNode) node).getValue());
+    } else {
+      throw new InvalidArgumentException(
+          "Do not expect other node than MappingNode or SequenceNode here "
+              + node.getClass().getName());
     }
+    parsedValue.setPrintable(true);
+    return parsedValue;
+  }
 }
