40215d7a4926599bfb3f4cf4e2c3ce263f49f8a5
Jose Antonio Sanchez <jose.sanchezm@atos.net>
2019-07-12 12:21:32 +0200

    Use new orchestrator client

--- alien4cloud-security/src/main/java/alien4cloud/security/spring/oidc/OidcConfig.java
+++ alien4cloud-security/src/main/java/alien4cloud/security/spring/oidc/OidcConfig.java
@@ -2,6 +2,7 @@ package alien4cloud.security.spring.oidc;
 
 import javax.annotation.Resource;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
@@ -16,10 +17,21 @@ import org.springframework.social.security.AuthenticationNameUserIdSource;
 
 import alien4cloud.security.users.IAlienUserDao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+@Slf4j
 @Configuration
 @EnableSocial
 @Profile("oidc-auth")
 public class OidcConfig implements SocialConfigurer {
+
+
+
     @Resource
     private IAlienUserDao alienUserDao;
 
@@ -30,8 +42,31 @@ public class OidcConfig implements SocialConfigurer {
 
     @Override
     public void addConnectionFactories(ConnectionFactoryConfigurer connectionFactoryConfigurer, Environment environment) {
-            connectionFactoryConfigurer.addConnectionFactory(
+        String orchestratorKeystorePath = environment.getProperty("deep.orchestrator.keystore.location");
+        String orchestratorKeystorePassword = environment.getProperty("deep.orchestrator.keystore.password");
+        KeyStore orchestratorKeystore = null;
+        if (orchestratorKeystorePath != null) {
+            try {
+                orchestratorKeystore = KeyStore.getInstance("JKS");
+                char[] keystorePassword = null;
+                if (orchestratorKeystorePassword != null) {
+                    keystorePassword = orchestratorKeystorePassword.toCharArray();
+                }
+                orchestratorKeystore.load(new FileInputStream(orchestratorKeystorePath), keystorePassword);
+            } catch (KeyStoreException e) {
+                log.error("Error creating keystore for the orchestrator certificate", e);
+            } catch (IOException e) {
+                log.error("Error loading orchestrator certificate keystore", e);
+            } catch (NoSuchAlgorithmException e) {
+                log.error("Invalid algorithm for orchestrator certificate keystore", e);
+            } catch (CertificateException e) {
+                log.error("Invalid orchestrator certificate", e);
+            }
+        }
+        connectionFactoryConfigurer.addConnectionFactory(
                     new OidcConnectionFactory(
+                            environment.getProperty("deep.orchestrator.path"),
+                            orchestratorKeystore,
                             environment.getProperty("oidc.iam.issuer"),
                             environment.getProperty("oidc.iam.client-id"),
                             environment.getProperty("oidc.iam.client-secret")));
