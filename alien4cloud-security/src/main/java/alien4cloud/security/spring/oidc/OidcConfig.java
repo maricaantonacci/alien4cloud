c2a93925cf0320a47ca55673b5e03663a864e402
Jose Antonio Sanchez <jose.sanchezm@atos.net>
2019-07-17 11:20:28 +0200

    Create factory for DeepOrchestrator just in case

--- alien4cloud-security/src/main/java/alien4cloud/security/spring/oidc/OidcConfig.java
+++ alien4cloud-security/src/main/java/alien4cloud/security/spring/oidc/OidcConfig.java
@@ -3,15 +3,21 @@ package alien4cloud.security.spring.oidc;
 import javax.annotation.Resource;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.env.Environment;
 import org.springframework.social.UserIdSource;
 import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurer;
+import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.oidc.deep.api.DeepOrchestrator;
 import org.springframework.social.oidc.deep.connect.OidcConnectionFactory;
 import org.springframework.social.security.AuthenticationNameUserIdSource;
 
@@ -70,6 +76,13 @@ public class OidcConfig implements SocialConfigurer {
                             environment.getProperty("oidc.iam.issuer"),
                             environment.getProperty("oidc.iam.client-id"),
                             environment.getProperty("oidc.iam.client-secret")));
+    }
+
+    @Bean
+    @Scope(value="request", proxyMode= ScopedProxyMode.INTERFACES)
+    public DeepOrchestrator orchestrator(ConnectionRepository repository) {
+        Connection<DeepOrchestrator> connection = repository.findPrimaryConnection(DeepOrchestrator.class);
+        return connection != null ? connection.getApi() : null;
     }
 
     @Override
