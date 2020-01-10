9dd0bea82f89211c478df228b616fa1a75d02cac
Andy S Alic <asalic@users.noreply.github.com>
2020-01-10 12:39:41 +0100

    Relationships in requirements fail if complex fix #44

--- alien4cloud-tosca/src/main/java/alien4cloud/tosca/parser/impl/advanced/RelationshipTemplateParser.java
+++ alien4cloud-tosca/src/main/java/alien4cloud/tosca/parser/impl/advanced/RelationshipTemplateParser.java
@@ -4,9 +4,11 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import alien4cloud.tosca.context.ToscaContext;
 import org.alien4cloud.tosca.model.definitions.AbstractPropertyValue;
 import org.alien4cloud.tosca.model.definitions.Interface;
 import org.alien4cloud.tosca.model.templates.RelationshipTemplate;
+import org.alien4cloud.tosca.model.types.RelationshipType;
 import org.springframework.stereotype.Component;
 import org.yaml.snakeyaml.nodes.MappingNode;
 import org.yaml.snakeyaml.nodes.Node;
@@ -61,7 +63,20 @@ public class RelationshipTemplateParser implements INodeParser<RelationshipTempl
                     relationshipTemplate.setTargetedCapabilityName(scalarParser.parse(nodeTuple.getValueNode(), context));
                     break;
                 case "relationship":
-                    relationshipTemplate.setType(scalarParser.parse(nodeTuple.getValueNode(), context));
+                    // We either have a short definition of a relationship (its type or a name referencing a relationship defined in the
+                    // relationship_template part of the topology) or a full blown definition of a relationship
+                    if (nodeTuple.getValueNode() instanceof ScalarNode) {
+                        String value = scalarParser.parse(nodeTuple.getValueNode(), context);
+                        RelationshipType relationshipType = ToscaContext.get(RelationshipType.class, value);
+                        if (relationshipType != null)
+                            relationshipTemplate.setType(value);
+                        else
+                            relationshipTemplate.setName(value);
+                    } else if (nodeTuple.getValueNode() instanceof MappingNode) {
+                        parseExistingRelationshipTemplate(relationshipTemplate, (MappingNode) nodeTuple.getValueNode(), context);
+                    } else {
+                        ParserUtils.addTypeError(node, context.getParsingErrors(), "Relationship template definition simple or complex");
+                    }
                     break;
                 case "properties":
                     INodeParser<AbstractPropertyValue> propertyValueParser = context.getRegistry().get("node_template_property");
@@ -83,4 +98,32 @@ public class RelationshipTemplateParser implements INodeParser<RelationshipTempl
 
         return relationshipTemplate;
     }
+
+    public void parseExistingRelationshipTemplate(RelationshipTemplate relationshipTemplate,
+                                                                  MappingNode node, ParsingContextExecution context) {
+        for (NodeTuple nodeTuple : node.getValue()) {
+            String key = scalarParser.parse(nodeTuple.getKeyNode(), context);
+            switch (key) {
+                case "type":
+                    relationshipTemplate.setType(scalarParser.parse(nodeTuple.getValueNode(), context));
+                    break;
+                case "capability":
+                    relationshipTemplate.setTargetedCapabilityName(scalarParser.parse(nodeTuple.getValueNode(), context));
+                    break;
+                case "properties":
+                    INodeParser<AbstractPropertyValue> propertyValueParser = context.getRegistry().get("node_template_property");
+                    MapParser<AbstractPropertyValue> mapParser = baseParserFactory.getMapParser(propertyValueParser, "node_template_property");
+                    relationshipTemplate.setProperties(mapParser.parse(nodeTuple.getValueNode(), context));
+                    break;
+                case "interfaces":
+                    INodeParser<Map<String, Interface>> interfacesParser = context.getRegistry().get("interfaces");
+                    relationshipTemplate.setInterfaces(interfacesParser.parse(nodeTuple.getValueNode(), context));
+                    break;
+                default:
+                    context.getParsingErrors().add(new ParsingError(ParsingErrorLevel.WARNING, ErrorCode.UNKNOWN_ARTIFACT_KEY, null, node.getStartMark(),
+                            "Unrecognized key while parsing implementation artifact", node.getEndMark(), key));
+            }
+        }
+    }
+
 }
